c42cf10a9ea4aa0f41158b54594d834a73d6e866
Noah Goldsmid <noahgoldsmid@live.nl>
2015-03-12 13:14:07 +0100

    Improved dynamic handling

--- src/protocol/DummyRoutingProtocol.java
+++ src/protocol/DummyRoutingProtocol.java
@@ -11,11 +11,11 @@ public class DummyRoutingProtocol implements IRoutingProtocol {
 	private LinkLayer linkLayer;
 	private ConcurrentHashMap<Integer, BasicRoute> forwardingTable = new ConcurrentHashMap<Integer, BasicRoute>();
 	private DataTable dataTable;
-
+    private DataTable[] neighboursTable;
 	@Override
 	public void init(LinkLayer linkLayer) {
 		this.linkLayer = linkLayer;
-
+        neighboursTable = new DataTable[CLIENTS];
 		System.out.println(linkLayer.getOwnAddress());
 
 		this.dataTable = new DataTable(3);
@@ -45,13 +45,14 @@ public class DummyRoutingProtocol implements IRoutingProtocol {
 				boolean isUpdated = false;
 				if (packet != null) {
 					DataTable data = packet.getData();
+                    neighboursTable[packet.getSourceAddress()] = data;
 					for (int i = 1; i < CLIENTS + 1; i++) {
 						Integer[] row = data.getRow(i);
+                        System.out.println("source:" + packet.getSourceAddress());
+                        System.out.println("remote " + i + ":" + Arrays.toString(row));
 						int dst = row[1] + dataTable.get(packet.getSourceAddress(), 1);
-						if (row[1] != -1 && (dst < dataTable.get(i, 1) || dataTable.get(i, 1) == -1 || dataTable.get(i, 2) == packet.getSourceAddress())) {
+						if (i != this.linkLayer.getOwnAddress() && row[1] != -1 && (dst < dataTable.get(i, 1) || dataTable.get(i, 1) == -1 || dataTable.get(i, 2) == packet.getSourceAddress())) {
 							if (dataTable.get(i, 1) != dst || dataTable.get(i, 2) != packet.getSourceAddress()) {
-								System.out.println("source:" + packet.getSourceAddress());
-								System.out.println("remote " + i + ":" + Arrays.toString(row));
 								System.out.println("local " + i + ":" + Arrays.toString(dataTable.getRow(i)));
 								isUpdated = true;
 								dataTable.set(i, 1, dst);
@@ -63,18 +64,30 @@ public class DummyRoutingProtocol implements IRoutingProtocol {
 				}
 				for (int i = 1; i < CLIENTS + 1; i++) {
 					int dst = this.linkLayer.getLinkCost(i);
-					if (dataTable.get(i, 2) == i) {
-						if (dataTable.get(i, 1) != dst) {
-							dataTable.set(i, 1, dst);
-							System.out.println("Link " + i + " updated to " + dst);
-							isUpdated = true;
-						}
-					}
-					if (dst != -1 && dst < dataTable.get(i, 1)) {
-						isUpdated = true;
-						dataTable.set(i, 1, dst);
-						dataTable.set(i, 2, i);
-					}
+                    int oldDst = dataTable.get(i, 1);
+                    if (dst != oldDst) {
+                        for (int j = 1; j < CLIENTS + 1; i++) {
+                            if (dataTable.get(j, 2) == i) {
+                                if (dst == -1) {
+                                    dataTable.set(j, 1, -1);
+                                    isUpdated = true;
+                                } else {
+                                    dataTable.set(j, 1, dataTable.get(j, 1) + dst - oldDst);
+                                    isUpdated = true;
+                                }
+                                for (int k = 1; k < CLIENTS + 1; i++) {
+                                    if (neighboursTable[k] != null &&
+                                            neighboursTable[k].get(j, 1) + dataTable.get(dataTable.get(k, 2), 1) < dataTable.get(j, 1) &&
+                                            neighboursTable[k].get(j, 1) != -1) {
+                                        dataTable.set(j, 1, neighboursTable[k].get(j, 1) + dataTable.get(dataTable.get(k, 2), 1));
+                                        dataTable.set(j, 2, k);
+                                        isUpdated = true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+
 				}
 				if (isUpdated) {
 					for (int i = 1; i < CLIENTS + 1; i++) {
